5372cf9d96d13ca4e2891edf2804738be11e5715
DDRAGON <davide.0829@gmail.com>
2022-08-24 20:17:55 +0900

    firebaseアプリの初期設定を追加

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDYel8y-2Sv3FMFdrl3gMNxvqv-D0M_0g",
+  authDomain: "ddragon-diary.firebaseapp.com",
+  projectId: "ddragon-diary",
+  storageBucket: "ddragon-diary.appspot.com",
+  messagingSenderId: "962514070156",
+  appId: "1:962514070156:web:b0033eafe6b4d0a00e29a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
